1efd1016c2378230e8679dcde3d7549a6b7c8515
andft <andrescarvajal1415@gmail.com>
2025-11-08 17:24:37 -0500

    pagos controllers y modules 2

--- routes/pagosroutes.js
+++ routes/pagosroutes.js
@@ -1,1 +1,34 @@
+import { Router } from "express";
+import {
+    getPagos,
+    getPagoUsuario,
+    postNuevoPago,
+    deletePago,
+    getEstadoUsuario
+} from "../controllers/pagoscontrollers.js";
+import {
+    validarPagoUsuario,
+    validarRegistro,
+    validarEliminacion,
+    validarEstadoUsuario
+} from "../validators/pagosvalidators.js";
+
+const router = Router();
+
+router.get("/api/pago", getPagos);
+// http://localhost:5040/api/pago
+
+router.get("/api/pago/:id", validarPagoUsuario, getPagoUsuario);
+// http://localhost:5040/api/pago/123
+
+router.post("/api/pago", validarRegistro, postNuevoPago);
+// http://localhost:5040/api/pago
+
+router.delete("/api/pago/:id", validarEliminacion, deletePago);
+// http://localhost:5040/api/pago/123
+
+router.get("/api/pago/:id", validarEstadoUsuario, getEstadoUsuario);
+// http://localhost:5040/api/usuarios/123
+
+export default router;
 
