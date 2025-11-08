3b131887bd0ae1e0cf4c2a6d86b06f6d5333d89c
andft <andrescarvajal1415@gmail.com>
2025-11-08 17:11:57 -0500

    pagos controllers y modules

--- controllers/pagoscontrollers.js
+++ controllers/pagoscontrollers.js
@@ -1,1 +1,58 @@
+import {
+    obtenerPagos,
+    obtenerPagosUsuario,
+    registrarPago,
+    eliminarPago,
+    verificarEstadoUsuario
+} from "../models/pagosmodels.js";
 
+export async function getPagos(req, res) {
+    try {
+        const pagos = await obtenerPagos()
+        res.json(pagos)
+    } catch (error) {
+       console.error("Error al obtener el pago:", error);     
+       res.status(500).json({ message: "Error al obtener los pagos"})
+    }
+}
+
+export async function getPagoUsuario(req, res) {
+    try {
+        const pago = await obtenerPagosUsuario(req.params.id)
+        res.json(pago)
+        if (!pago) return res.status(404).json({ error: 'Pago no encontrado' });
+    } catch (error) {
+        console.error("Pago no encontrado")
+        res.status(500).json({message: "Error al obtener el pago"})
+    }
+}
+
+export async function postNuevoPago(req, res) {
+  try {
+    const nuevoPago = await registrarPago(req.body);
+    res.status(201).json(nuevoPago);
+  } catch (error) {
+    console.error("Error al registrar el pago:", error);
+    res.status(500).json({ message: "Error al registrar el pago" });
+  }
+}
+
+export async function deletePago(req, res) {
+  try {
+    const pago = await eliminarPago(req.params.id);
+    res.json(pago);
+  } catch (error) {
+    console.error("Error al eliminar el pago:", error);
+    res.status(500).json({ message: "Error al eliminar el pago" });
+  }
+}
+
+export async function getEstadoUsuario(req, res) {
+  try {
+    const estado = await verificarEstadoUsuario(req.params.id)
+    res.json(estado)
+  } catch (error) {
+    console.error("Error al verificar el estado del usuario")
+    res.status(500).json({ message: "Error al verificar el estado del usuario" })
+  }
+}
--- models/pagosmodels.js
+++ models/pagosmodels.js
@@ -1,1 +1,66 @@
+import pool from "../config/db.js"
+
+export async function obtenerPagos() {
+    const [rows] = await pool.query(`
+        select id as id_pagos, fecha_pago
+        from pagos 
+        order by fecha_pago desc
+    `);
+    return rows;
+}
+
+export async function obtenerPagosUsuario(usuarioId) {
+  const [rows] = await pool.query(`
+    select 
+      id AS id_pago,
+      monto,
+      fecha_pago,
+      fecha_vencimiento,
+      metodo
+    from pagos
+    where usuario_id = ?
+    order by fecha_pago desc
+  `, [usuarioId]);
+  return rows;
+}
+
+export async function registrarPago({usuarioId, monto, fecha_pago, fecha_vencimiento, metodo}) {
+    const [result] = await pool.query(`
+        insert into pagos (usuario_id, monto, fecha_pago, fecha_vencimiento, metodo)
+        values (?, ?, ?, ?, ?),
+        [usuario_id, monto, fecha_pago, fecha_vencimiento, metodo]
+    `);
+    return {
+        id: result.insertId,
+        usuarioId,
+        monto,
+        fecha_pago,
+        fecha_vencimiento,
+        metodo,
+    };
+}
+
+export async function eliminarPago(id) {
+    await pool.query(`
+        delete from pagos where id = ?    
+    ` [id]);
+    return {mensaje: "Pago eliminado"}
+}
+
+export async function verificarEstadoUsuario(usuario_id) {
+  const [rows] = await pool.query(
+    `select 
+       case 
+         when fecha_vencimiento >= curdate() then 'activo'
+         else 'vencido'
+       end as estado
+     from pagos
+     where usuario_id = ?
+     order by fecha_vencimiento desc
+     limit 1`,
+    [usuario_id]
+  );
+
+  return rows[0] || { estado: 'sin pagos' };
+}
 
