8e3f57643fd8a99910138ba9435c705c158baf0b
dfaparicio <dfapariciocastro13@gmail.com>
2025-11-10 21:55:54 -0500

    Nuevas actualizaciones en lecturas

--- controllers/lecturascontrollers.js
+++ controllers/lecturascontrollers.js
@@ -8,7 +8,7 @@ import {
 /************** IA ***************/
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 export async function respuestaIA(prompt) {
